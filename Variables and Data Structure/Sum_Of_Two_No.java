dd16336ddcc50577813f7e0abf0cae913d994b42
dhananjay421366 <nimbalkardhananjay349@gmail.com>
2025-03-26 21:11:19 +0530

    controll statement was doen

--- Variables and Data Structure/Sum_Of_Two_No.java
+++
@@ -1,8 +0,0 @@
-public class Sum_Of_Two_No {
-    public static void main(String args[]) {
-        int a = 10;
-        int b = 20;
-        int sum = a + b;
-        System.out.println(sum);
-    }
-}
